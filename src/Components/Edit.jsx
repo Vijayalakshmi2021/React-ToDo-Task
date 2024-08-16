8dcaa8b1800d3b3846c6fa9b84362f909ab2a0ef
Vijayalakshmi2021 <sviji1792@gmail.com>
2024-08-17 05:10:39 +0530

    React ToDo Task

---
+++ src/Components/Edit.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function Edit({ title, setTitle, description, setDescription, onClick }) {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Edit Todo</h2>
+      <div style={{ marginBottom: 10 }}>
+        <label style={{ display: 'block', marginBottom: 5 }}>Name:</label>
+        <input
+          type="text"
+          value={title}
+          placeholder='Ur Name'
+          onChange={(e) => setTitle(e.target.value)}
+          style={{ width: '50%', padding: '8px', marginBottom: 10 }}
+        />
+      </div>
+      <div style={{ marginBottom: 10 }}>
+        <label style={{ display: 'block', marginBottom: 5 }}>Description:</label>
+        <input
+          type="text"
+          value={description}
+          placeholder='Ur Description'
+          onChange={(e) => setDescription(e.target.value)}
+          style={{ width: '50%', padding: '8px', marginBottom: 10 }}
+        />
+      </div>
+      <button onClick={onClick} style={{ padding: '8px', margin: '10px 0', background: 'green', color: 'white', border: 'none', cursor: 'pointer' }}>
+        Save Changes
+      </button>
+    </div>
+  );
+}
+
+export default Edit;
